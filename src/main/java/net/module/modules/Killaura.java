559fdcda5eb3012a9e208d0ffb727a0ea6b3ed9b
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:44:45 +0700

    Implement KillAura module with enable/disable methods

--- src/main/java/net/module/modules/Killaura.java
+++ src/main/java/net/module/modules/Killaura.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class KillAura extends Module {
+    public KillAura() {
+        super("KillAura", "Combat");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("KillAura enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("KillAura disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
