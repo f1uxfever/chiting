2210bcfe6a554ecf9a3421b080cd226bf78bba45
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:42:57 +0700

    Add Fly module with basic functionality
    Implement Fly module with enable and disable methods.

--- src/main/java/net/module/modules/Fly.java
+++ src/main/java/net/module/modules/Fly.java
@@ -1,1 +1,24 @@
 
+package net.cheatmenu.module.modules;
+
+import net.cheatmenu.module.Module;
+
+public class Fly extends Module {
+    public Fly() {
+        super("Fly", "Movement");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Fly enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Fly disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
