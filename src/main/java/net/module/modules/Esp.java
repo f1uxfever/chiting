56c0099fd57a92d9af8f55e5a3cf9b92fc6e0940
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:41:49 +0700

    Implement ESP module with enable/disable methods

--- src/main/java/net/module/modules/Esp.java
+++ src/main/java/net/module/modules/Esp.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class ESP extends Module {
+    public ESP() {
+        super("ESP", "Render");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("ESP enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("ESP disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
