fd69c0bed3aae0aace8c57952d165cd27b29d3cb
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:46:05 +0700

    Update Speed.java

--- src/main/java/net/module/modules/Speed.java
+++ src/main/java/net/module/modules/Speed.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class Speed extends Module {
+    public Speed() {
+        super("Speed", "Movement");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Speed enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Speed disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
