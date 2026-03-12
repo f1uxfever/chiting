1973ddb3454b2a2d98b7b27b7e3860d48f0efd3c
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:46:30 +0700

    Implement Timer module with enable/disable logging

--- src/main/java/net/module/modules/Timer.java
+++ src/main/java/net/module/modules/Timer.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class Timer extends Module {
+    public Timer() {
+        super("Timer", "Misc");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Timer enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Timer disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
