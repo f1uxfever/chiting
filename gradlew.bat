9eef6cabdebe05d0c10494f691fdeb6650ad8887
f1uxfever <hhona5866@gmail.com>
2026-03-12 13:06:58 +0700

    Create gradlew.bat for Gradle wrapper
    Add gradlew.bat for Windows Gradle wrapper support

---
+++ gradlew.bat
@@ -0,0 +1,14 @@
+@echo off
+java -jar "%~dp0gradlew.jar" %*
+```
+4. **Нажми** "Commit new file"
+
+---
+
+### ⚠️ ВАЖНО!
+
+Эти файлы должны быть в **корне репозитория**, рядом с:
+```
+build.gradle
+gradle.properties
+settings.gradle
