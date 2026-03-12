70f14fe47677e4b4e3100841d565b6b0cb09f564
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:41:24 +0700

    Add Coordinates module with onEnable and onDisable methods
    Implement Coordinates module with enable/disable logging.

--- src/main/java/net/module/modules/Coordinates.java
+++ src/main/java/net/module/modules/Coordinates.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class Coordinates extends Module {
+    public Coordinates() {
+        super("Coordinates", "Render");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Coordinates enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Coordinates disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
