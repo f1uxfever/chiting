2ae8842aaabd5c00dc32cc4a4b1d3c124666b654
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:45:39 +0700

    Implement NoFall module with enable/disable messages

--- src/main/java/net/module/modules/NoFall.java
+++ src/main/java/net/module/modules/NoFall.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class NoFall extends Module {
+    public NoFall() {
+        super("NoFall", "Movement");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("NoFall enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("NoFall disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
