90321f41fcf24655c81c7abf10576fbfad6cbd15
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:47:12 +0700

    Implement Tracers module with enable/disable methods

--- src/main/java/net/module/modules/Tracercs.java
+++ src/main/java/net/module/modules/Tracercs.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class Tracers extends Module {
+    public Tracers() {
+        super("Tracers", "Render");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Tracers enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Tracers disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
