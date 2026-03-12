64272cc1f68434a72d8d92c6a6288e89a2404c87
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:43:20 +0700

    Implement FullBright module with enable/disable methods

--- src/main/java/net/module/modules/FullBright.java
+++ src/main/java/net/module/modules/FullBright.java
@@ -1,1 +1,24 @@
 
+package net.cheatmenu.module.modules;
+
+import net.cheatmenu.module.Module;
+
+public class FullBright extends Module {
+    public FullBright() {
+        super("FullBright", "Render");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("FullBright enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("FullBright disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
