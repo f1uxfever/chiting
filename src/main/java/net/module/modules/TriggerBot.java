ba0f10a5f2411de565a47519f1c7af33433ac8ac
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:47:35 +0700

    Implement TriggerBot module with enable/disable methods

--- src/main/java/net/module/modules/TriggerBot.java
+++ src/main/java/net/module/modules/TriggerBot.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class TriggerBot extends Module {
+    public TriggerBot() {
+        super("TriggerBot", "Combat");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("TriggerBot enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("TriggerBot disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
