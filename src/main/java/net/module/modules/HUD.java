7bb584974b3b89965291c6cecc073d6ab50901ed
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:44:10 +0700

    Implement HUD module with enable/disable logging

--- src/main/java/net/module/modules/HUD.java
+++ src/main/java/net/module/modules/HUD.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class HUD extends Module {
+    public HUD() {
+        super("HUD", "Render");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("HUD enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("HUD disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
