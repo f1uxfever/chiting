1cb3454c76735645da4773fbb4b27d45982dc97a
f1uxfever <hhona5866@gmail.com>
2026-03-12 12:40:49 +0700

    Update ChatSpam.java

--- src/main/java/net/module/modules/ChatSpam.java
+++ src/main/java/net/module/modules/ChatSpam.java
@@ -1,1 +1,23 @@
+package net.cheatmenu.module.modules;
 
+import net.cheatmenu.module.Module;
+
+public class ChatSpam extends Module {
+    public ChatSpam() {
+        super("ChatSpam", "Misc");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("ChatSpam enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("ChatSpam disabled");
+    }
+
+    @Override
+    public void onTick() {
+    }
+}
